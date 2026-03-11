Fix normalization wrappers

memorax/environments/wrappers/normalize_observation.py
@@ -11,7 +11,7 @@ from gymnax.wrappers.purerl import GymnaxWrapper
 @struct.dataclass
 class NormalizeObservationWrapperState:
     mean: jnp.ndarray
-    var: jnp.ndarray
+    M2: jnp.ndarray
     count: float
     env_state: environment.EnvState
 
@@ -21,29 +21,29 @@ class NormalizeObservationWrapper(GymnaxWrapper):
         super().__init__(env)
         self.eps = eps
 
-    def _welford_update(self, mean, var, count, obs):
+    def _welford_update(self, mean, M2, count, obs):
         count = count + 1
         delta = obs - mean
         mean = mean + delta / count
         delta2 = obs - mean
-        var = var + (delta * delta2 - var) / count
-        return mean, var, count
+        M2 = M2 + delta * delta2
+        return mean, M2, count
 
     def reset(
         self, key: Key, params: Optional[environment.EnvParams] = None
     ) -> Tuple[Array, NormalizeObservationWrapperState]:
         obs, env_state = self._env.reset(key, params)
         mean = jnp.zeros_like(obs)
-        var = jnp.ones_like(obs)
-        count = self.eps
-        mean, var, count = self._welford_update(mean, var, count, obs)
+        M2 = jnp.ones_like(obs)
+        count = 1.0
+        mean, M2, count = self._welford_update(mean, M2, count, obs)
         state = NormalizeObservationWrapperState(
             mean=mean,
-            var=var,
+            M2=M2,
             count=count,
             env_state=env_state,
         )
-        return (obs - mean) / jnp.sqrt(var + self.eps), state
+        return (obs - mean) / jnp.sqrt(M2 / count + self.eps), state
 
     def step(
         self,
@@ -55,17 +55,17 @@ class NormalizeObservationWrapper(GymnaxWrapper):
         obs, env_state, reward, done, info = self._env.step(
             key, state.env_state, action, params
         )
-        mean, var, count = self._welford_update(
-            state.mean, state.var, state.count, obs
+        mean, M2, count = self._welford_update(
+            state.mean, state.M2, state.count, obs
         )
         state = NormalizeObservationWrapperState(
             mean=mean,
-            var=var,
+            M2=M2,
             count=count,
             env_state=env_state,
         )
         return (
-            (obs - state.mean) / jnp.sqrt(state.var + self.eps),
+            (obs - state.mean) / jnp.sqrt(state.M2 / state.count + self.eps),
             state,
             reward,
             done,

memorax/environments/wrappers/normalize_reward.py
@@ -29,8 +29,8 @@ class NormalizeRewardWrapper(GymnaxWrapper):
         obs, env_state = self._env.reset(key, params)
         state = NormalizeRewardWrapperState(
             mean=0.0,
-            M2=0.0,
-            count=0.0,
+            M2=1.0,
+            count=1.0,
             G=0.0,
             env_state=env_state,
         )
@@ -54,9 +54,7 @@ class NormalizeRewardWrapper(GymnaxWrapper):
         mean = state.mean + delta / count
         delta2 = G - mean
         M2 = state.M2 + delta * delta2
-        var = M2 / count
-
-        scaled_reward = reward / jnp.sqrt(var + self.eps)
+        scaled_reward = reward / jnp.sqrt(M2 / count + self.eps)
 
         new_state = NormalizeRewardWrapperState(
             mean=mean,